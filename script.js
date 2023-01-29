da9c82af3d1f95a225eac57668517ce8fbe7fc14
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-01-29 15:04:31 -0300

    :tada: relogio

--- script.js
+++ script.js
@@ -5,64 +5,38 @@ const controles = {
     "ct3_1": document.getElementById('opc_pomo')
 }
 
-// var radios = document.getElementsByName('tipo');
-//   for(radio in radios) {
-//     radios[radio].onclick = function() {
-//         switch(this.value){
-//             case '1':
-//                 (controles.ct1, controles.ct2, controles.ct3, controles.ct3_1).style.opacity = "0";
-//                 (controles.ct1, controles.ct2, controles.ct3, controles.ct3_1).style.visibility = "hidden";
-//                 document.getElementById('txtR').style.opacity = '1';
-//                 break
-//             case '2':
-//                 document.getElementById('cnt1').style.opacity = '0';
-//                 document.getElementById('cnt1').style.visibility = 'hidden';
-//                 document.getElementById('cnt2').style.opacity = '1';
-//                 document.getElementById('cnt2').style.visibility = 'visible';
-//                 break
-//             case '3':
-//                 controles.ct3_1.style.visibility = "hidden";
-//                 controles.ct3_1.style.opacity = "0";
-//                 document.getElementById('txtR').style.opacity = "0";
-//                 (controles.ct2, controles.ct3).style.opacity = "0";
-//                 (controles.ct2, controles.ct3).style.visibility = "hidden";
-//                 controles.ct1.style.visibility = 'visible';
-//                 controles.ct1.style.opacity = '1';
-//                 break
-//             case '4':
-//                 controles.ct3_1.style.visibility = "hidden";
-//                 controles.ct3_1.style.opacity = "0";
-//                 document.getElementById('txtR').style.opacity = "0";
-//                 (controles.ct1, controles.ct3).style.opacity = "0";
-//                 (controles.ct1, controles.ct3).style.visibility = "hidden";
-//                 controles.ct2.style.visibility = "visible";
-//                 controles.ct2.style.opacity = "1";
-//                 break
-//             case '5':
-//                 document.getElementById('txtR').style.opacity = "0";
-//                 (controles.ct1, controles.ct2).style.opacity = "0";
-//                 (controles.ct1, controles.ct2).style.visibility = "hidden";
-//                 controles.ct3.style.visibility = "visible";
-//                 controles.ct3.style.opacity = "1";
-//                 controles.ct3_1.style.visibility = "visible";
-//                 controles.ct3_1.style.opacity = "1";
-//                 break
-//         }
-//     }
-// }
+const num = { 
+    horas: document.getElementById('H_M'),
+    minutos: document.getElementById('M_S'),
+    segundos: document.getElementById('S_Ms')
+}
+
+function esconderControles(nome1, nome2, nome3){
+    controles[nome1].style.opacity = '0';
+    controles[nome1].style.visibility = 'hidden';
+    controles[nome2].style.opacity = '0';
+    controles[nome2].style.visibility = 'hidden';
+    controles[nome3].style.opacity = '0';
+    controles[nome3].style.visibility = 'hidden';
+}
+
+function zerarRelogio(){
+    clearInterval(relogioI);
+    num.horas.innerHTML = '00'
+    num.minutos.innerHTML = '00'
+    num.segundos.innerHTML = '00'
+}
 
 function relogio(){
 
-    controles.ct1.style.opacity = '0';
-    controles.ct1.style.visibility = 'hidden';
-    controles.ct2.style.opacity = '0';
-    controles.ct2.style.visibility = 'hidden';
-    controles.ct3.style.opacity = '0';
-    controles.ct3.style.visibility = 'hidden';
+    esconderControles('ct1', 'ct2', 'ct3')
+    
     controles.ct3_1.style.visibility = 'hidden';
     controles.ct3_1.style.opacity = '0';
 
     document.getElementById('txtR').style.opacity = '1';
+
+    relogioI = setInterval(displayTime, 1000)
 
 }
 
@@ -70,15 +44,12 @@ function cronometro(){
 
     document.getElementById('txtR').style.opacity = '0';
 
-    controles.ct2.style.opacity = '0';
-    controles.ct2.style.visibility = 'hidden';
-    controles.ct3.style.opacity = '0';
-    controles.ct3.style.visibility = 'hidden';
-    controles.ct3_1.style.visibility = 'hidden';
-    controles.ct3_1.style.opacity = '0';
+    esconderControles('ct2', 'ct3', 'ct3_1')
 
     controles.ct1.style.visibility = 'visible';
     controles.ct1.style.opacity = '1';
+
+    zerarRelogio()
 
 }
 
@@ -86,16 +57,12 @@ function timer(){
 
     document.getElementById('txtR').style.opacity = '0';
 
-    controles.ct1.style.opacity = '0';
-    controles.ct1.style.visibility = 'hidden';
-    controles.ct3.style.opacity = '0';
-    controles.ct3.style.visibility = 'hidden';
-    controles.ct3_1.style.visibility = 'hidden';
-    controles.ct3_1.style.opacity = '0';
+    esconderControles('ct1', 'ct3', 'ct3_1')
 
     controles.ct2.style.visibility = 'visible';
     controles.ct2.style.opacity = '1';
 
+    zerarRelogio()
 
 }
 
@@ -108,9 +75,108 @@ function pomodoro(){
     controles.ct2.style.opacity = '0';
     controles.ct2.style.visibility = 'hidden';
 
+    esconderControles('ct1', 'ct2', 'ct2');
+
     controles.ct3_1.style.visibility = 'visible';
     controles.ct3_1.style.opacity = '1';
     controles.ct3.style.visibility = 'visible';
     controles.ct3.style.opacity = '1';
 
+    zerarRelogio()
+
 }
+
+function displayTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    if (hours < 10){
+        document.getElementById('H_M').innerHTML = '0' + hours;
+    }else{
+        document.getElementById('H_M').innerHTML = hours
+    }
+
+    if(minutes < 10){
+        document.getElementById('M_S').innerHTML = '0' + minutes
+    }else{
+        document.getElementById('M_S').innerHTML = minutes
+    }
+
+    if(seconds < 10){
+        document.getElementById('S_Ms').innerHTML = '0' + seconds
+    }else{
+        document.getElementById('S_Ms').innerHTML = seconds
+
+    }
+}
+  
+let relogioI = setInterval(displayTime, 1000);
+  
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+function startCron() {
+    startTime = new Date();
+    timerInterval = setInterval(updateCron, 10);
+  }
+  function updateCron() {
+    let currentTime = new Date();
+    elapsedTime = currentTime - startTime;
+    displayCron();
+  }
+  function displayCron() {
+    let time = new Date(elapsedTime);
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let milliseconds = time.getMilliseconds();
+
+    if (minutes < 10){
+        document.getElementById('H_M').innerHTML = '0' + minutes;
+    }else{
+        document.getElementById('H_M').innerHTML = minutes
+    }
+
+    if(seconds < 10){
+        document.getElementById('M_S').innerHTML = '0' + seconds
+    }else{
+        document.getElementById('M_S').innerHTML = seconds
+    }
+
+    if(milliseconds < 10){
+        document.getElementById('S_Ms').innerHTML = '00' + milliseconds
+
+    } else if(milliseconds < 100){
+        document.getElementById('S_Ms').innerHTML = '0' + milliseconds
+
+    }else{
+        document.getElementById('S_Ms').innerHTML = milliseconds
+
+    }
+  }
+
+function pauseCron() {
+    clearInterval(timerInterval);
+}
+
+let vCod = 0;
+function volta(){
+        if(vCod == 0){
+            clearInterval(timerInterval);
+            vCod = 1;
+        }
+        else{
+            timerInterval = setInterval(updateCron, 10);
+            vCod = 0;
+        }
+}
+
+function resetCron() {
+    clearInterval(timerInterval);
+    elapsedTime = 0;
+    displayCron();
+}
+  
